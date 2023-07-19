c1e974afd098e935550129d3fb7b65319f6724cd
Srinivas Hemadri <srinivashemadri2000@gmail.com>
2023-07-19 23:56:51 +0530

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () =>{
+    return(
+        <footer class="page-footer">
+          <div class="footer-copyright">
+            <div class="container">
+            © 2023 Srinivas
+            <p>Developer by Srinivas Hemadri</p>
+            </div>
+          </div>
+        </footer>
+    )
+}
+
+export default Footer;
